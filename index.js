339196a021b1d0a7ccdd0af5bc8d2a2c6788230f
Ana Ruiz <ana.ruizfz@udlap.mx>
2021-02-06 14:00:23 -0600

    Se corrigieron estilos de los botones

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ input.placeholder = 'New Task...';
 input.className = 'form-control';
 //create button submit
 var buttonSubmit = document.createElement('button');
-buttonSubmit.className = 'btn btn-outline-primary';
-var addIcon = document.createElement('i');
-addIcon.className = 'bi bi-plus text-light align-middle';
-addIcon.style.fontSize = '20px';
-buttonSubmit.append(addIcon);
+buttonSubmit.className = 'd-flex align-items-center btn btn-outline-primary bi bi-plus-circle text-primary';
+buttonSubmit.onmouseout = function() {
+  buttonSubmit.className = 'd-flex align-items-center btn btn-outline-primary bi bi-plus-circle text-primary';
+}
+buttonSubmit.onmouseover = function() {
+  buttonSubmit.className = 'd-flex align-items-center btn btn-outline-primary bi bi-plus-circle text-light';
+}
 //create form to input and buttonsubmit
 var taskForm = document.createElement('div');
 taskForm.className = 'input-group mt-4';
@@ -29,16 +31,15 @@ container.append(title, taskForm, tasksContainer);
 // FUNCTIONALITY
 buttonSubmit.addEventListener('click', function() {
   var taskContainer = document.createElement('div');
-  taskContainer.className = 'd-flex justify-content-between align-items-center mt-3';
+  taskContainer.className = 'd-flex justify-content-between align-items-center mt-3 bg-success';
   //create all task elements
   var taskCheckbox = document.createElement('input');
   taskCheckbox.type = 'checkbox';
   taskCheckbox.className = 'form-check-input';
-  var taskTitle = document.createElement('p');
+  var taskTitle = document.createElement('span');
   taskTitle.textContent = input.value;
   var taskDelete = document.createElement('button');
-  taskDelete.textContent = '❌';
-  taskDelete.className = 'btn btn-dark';
+  taskDelete.className = 'd-flex align-items-center btn btn-danger bi bi-trash';
 
   //click event to checkbox
   taskCheckbox.addEventListener('click', function(){
