1a805395f3f2803b226d59aef4f76ef13a62d089
Ana Ruiz <ana.ruizfz@udlap.mx>
2021-02-06 19:32:27 -0600

    Se añado sistema de prioridad

--- index.js
+++ index.js
@@ -29,6 +29,36 @@ tasksContainer.className = 'mt-4';
 container.append(title, taskForm, tasksContainer);
 
 // FUNCTIONALITY
+
+//preload tunes
+var completedTaskTune = document.createElement('audio');
+var addedTaskTune = document.createElement('audio');
+var deletedTaskTune = document.createElement('audio');
+var priorityTune = document.createElement('audio');
+
+function addAudioFileSource(audio,fileName, type) {
+  var tune = document.createElement('source');
+  tune.src = 'media/' + fileName + '.' + type;
+  tune.type = 'audio/' + type;
+  audio.append(tune);
+}
+
+addAudioFileSource(completedTaskTune,'achievement','ogg');
+addAudioFileSource(completedTaskTune,'achievement','mp3');
+
+addAudioFileSource(addedTaskTune,'add','ogg');
+addAudioFileSource(addedTaskTune,'add','mp3');
+
+addAudioFileSource(deletedTaskTune,'delete','ogg');
+addAudioFileSource(deletedTaskTune,'delete','mp3');
+
+addAudioFileSource(priorityTune,'priority','ogg');
+addAudioFileSource(priorityTune,'priority','mp3');
+
+function playAudio(audio) {
+  audio.play();
+}
+
 buttonSubmit.addEventListener('click', function() {
   var task = document.createElement('div');
   task.className = 'd-flex justify-content-between align-items-center mt-3';
@@ -42,53 +72,57 @@ buttonSubmit.addEventListener('click', function() {
 
   //task editor
   var taskEditor = document.createElement('div'); 
-  taskEditor.className = 'd-flex justify-content-around align-self-middle align-items-center w-25'
+  taskEditor.className = 'd-flex justify-content-between align-self-middle align-items-center w-25'
     //delete task
   var taskDelete = document.createElement('button');
-  taskDelete.className = 'd-flex align-items-center btn btn-danger bi bi-trash';
+  taskDelete.className = 'd-flex align-items-center btn btn-danger bi bi-trash p-2';
     // select task priority
-  //var taskPriority = document.createElement('div');
-  /*
-  var prioritySelector = document.createElement('button');
-  prioritySelector.className = 'btn border-secondary text-secondary';
-  prioritySelector.textContent = 'Priority';
-  prioritySelector.addEventListener('click', function(){
-    var prioritySelectorClicked = aText.toggle('1');
-    if(prioritySelectorClicked)
-      console.log('1 click!');
-    else
-      console.log('2 click!');
+  var taskPriority = document.createElement('div');
+  taskPriority.className = 'd-flex justify-content-between align-self-middle align-items-center w-50'
+  
+  var prioritySelector = document.createElement('div');
+  prioritySelector.className = 'd-flex align-items-center border border-secondary rounded p-0';
+
+  var priorityLevel = 1;
+  var priorityColors = ['text-success','text-warning','text-danger'];
+
+  
+  
+  function raisePriority(container, level) {
+    container.innerHTML = '';
+    for(var i = 0; i < level; i++){
+      var element = document.createElement('div');
+      element.className = 'btn bi bi-exclamation-circle-fill align-self-top p-1 ' + priorityColors[level-1];
+      console.log(element.className);
+      container.append(element);
+    }
+  }
+  
+  function restartPriority(container) {
+    container.innerHTML = '';
+    raisePriority(container, 1);
+  }
+
+  //initiate  level 1 priority 
+  raisePriority(prioritySelector,1);
+
+  prioritySelector.addEventListener('click', function() {
+    priorityLevel++;
+    if(priorityLevel < 4)
+      raisePriority(prioritySelector,priorityLevel);
+    else {
+      restartPriority(prioritySelector);
+      priorityLevel = 1;
+    }
+    playAudio(priorityTune);
   })
-  */
-  taskEditor.append(taskDelete);
 
-  //preload tunes
-  var completedTaskTune = document.createElement('audio');
-  var addedTaskTune = document.createElement('audio');
-  var deletedTaskTune = document.createElement('audio');
   
-  function addAudioFileSource(audio,fileName, type) {
-    var tune = document.createElement('source');
-    tune.src = 'media/' + fileName + '.' + type;
-    tune.type = 'audio/' + type;
-    audio.append(tune);
-  }
-  
-  addAudioFileSource(completedTaskTune,'achievement','ogg');
-  addAudioFileSource(completedTaskTune,'achievement','mp3');
-  console.log(completedTaskTune);
+  taskPriority.append(document.createElement('span').textContent = 'Priority:',prioritySelector);
 
-  addAudioFileSource(addedTaskTune,'add','ogg');
-  addAudioFileSource(addedTaskTune,'add','mp3');
-  console.log(addedTaskTune);
-  
-  addAudioFileSource(deletedTaskTune,'delete','ogg');
-  addAudioFileSource(deletedTaskTune,'delete','mp3');
-  console.log(deletedTaskTune);
+  taskEditor.append(taskPriority,taskDelete);
 
-  function playAudio(audio) {
-    audio.play();
-  }
+  
 
   //sort tasks
   function sortTasks(container) {
